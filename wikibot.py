7f6e33fa21174998296a04672cb3c4fc932175e5
Yaribar <40440445+Yaribar@users.noreply.github.com>
2025-10-25 21:35:59 +0000

    fixed linter issues

--- wikibot.py
+++ wikibot.py
@@ -5,7 +5,7 @@ from mylib.bot import scrape
 @click.command()
 @click.option("--name", help="Web page we want to scrape")
 @click.option("--length", help="length of the output from wikipedia")
-def cli(name, length):
+def cli(name='Microsoft', length=1):
     result = scrape(name, length)
     click.echo(click.style(f"{result}:", fg="white"))
 
